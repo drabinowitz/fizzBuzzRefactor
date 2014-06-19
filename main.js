0b5dfa867f99c3d091c0404c59dd9357f84f734f
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-06-19 17:24:18 -0400

    fizzBuzzRefactor

--- main.js
+++ main.js
@@ -1,31 +1,47 @@
+var fizz = "fizz";
+var buzz = "buzz";
+
 $(document).ready(function(){
-	var fizz = "fizz"
-	var buzz = "buzz"
+ 
+ fizzInput(+prompt('enter fizz number'))
 
-	for(var i = 1;i<=100;i++){
+})
 
-		if(!(i % 5)){
+var fizzInput = function (inputVal){
 
-			if(!(i % 3)){
+	if (inputVal && Math.floor(inputVal) == inputVal){
 
-				$('body').append('<p>').append(fizz+buzz);
+		for(var i = 1;i<=inputVal;i++){
+
+			if(!(i % 5)){
+
+				if(!(i % 3)){
+
+					$('body').append('<p>').append(fizz+buzz);
+
+				} else {
+
+					$('body').append('<p>').append(buzz);
+
+				}
+
+			} else if(!(i % 3)){
+
+				$('body').append('<p>').append(fizz);
 
 			} else {
 
-				$('body').append('<p>').append(buzz);
-
+				$('body').append('<p>').append(i);
+				
 			}
 
-		} else if(!(i % 3)){
-
-			$('body').append('<p>').append(fizz);
-
-		} else {
-
-			$('body').append('<p>').append(i);
-			
 		}
+
+	} else {
+
+		alert('must enter an integer value!');
+		location.reload();
 
 	}
 
-})
+}
